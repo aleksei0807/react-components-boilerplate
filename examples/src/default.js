feff0b7c6e2f9a203eeb7a9b55dd66a4b2055f2a
Алексей <a@guava.by>
2016-06-02 03:30:36 +0300

    add pages

--- examples/src/default.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import CustomComponent from '../../lib/index';
-
-ReactDOM.render(
-	<CustomComponent />,
-	document.getElementById('root')
-);
